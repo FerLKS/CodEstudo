4205a35b7ebb50ca95e33bcd8c84c7329012863d
Fernando <ferlucas11@outlook.com>
2021-10-13 16:44:20 -0300

    Adicionado um bom dia

--- git.teste/Program.cs
+++ git.teste/Program.cs
@@ -7,6 +7,8 @@ namespace git.teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Bom dia!");
         }
     }
 }
